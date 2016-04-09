30effd29b22160ca04ec4af03a908a008183139b
Adam Cohen <atcohen2@illinois.edu>
2016-04-08 23:21:55 -0500

    do it

--- minimax.py
+++ minimax.py
@@ -28,7 +28,7 @@ class Minimax(object):
         """ Returns the best move (as a column number) and the associated alpha
             Calls search()
         """
-        print("STACK ", depth, curr_player)
+        #print("STACK ", depth, curr_player)
 
         
         # determine opponent's color
@@ -83,11 +83,11 @@ class Minimax(object):
             v = self.value(state, curr_player)
             #self.debug_value(state, curr_player)
             
-            print(v, depth)
+            '''print(v, depth)
             for line in reversed(state):
                 print line
             print("")
-            time.sleep(1)
+            time.sleep(1)'''
             
             #print v, depth
             #print state
@@ -151,7 +151,7 @@ class Minimax(object):
         if color == 'o':
             state, flipped_state = flipped_state, state
             
-        self.eval.debug_evaluate(state, flipped_state, color)
+        #self.eval.debug_evaluate(state, flipped_state, color)
 
     def value(self, state, color):
         # right side up
